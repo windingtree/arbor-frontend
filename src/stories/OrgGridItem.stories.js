21200fbe4d9ae410681bfed99f5fbe53b70520a3
Mariia Ilyukhina <killerdance7@gmail.com>
2020-01-27 20:08:42 +0200

    add Org Grid Item to storybook

---
+++ src/stories/OrgGridItem.stories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { storiesOf } from '@storybook/react';
+import {withKnobs, text, boolean, number, array} from '@storybook/addon-knobs';
+
+import OrgsGridItem from '../components/OrgsGridItem';
+
+storiesOf( 'ORG ID/Grid Item', module)
+  .addDecorator(withKnobs)
+  .add('Organization Grid Item',() => {
+    const name = text('Organization name', 'Default organization');
+    const id = text('id', '0x6584993ddddki888');
+    const isSub = boolean('SubOrganization', true);
+    const type = text('SubOrganization type', 'Hotel');
+    const subs = array('subs', ['1', 'a', '0xpps'], ', ');
+    const trustLevel = number('Trust level', 4);
+    const entityName = text('Legal entity name', 'Default corp.');
+    const entityTrustLevel = number('Entity Trust level', 5) ;
+
+    //TODO: find how add fonts to storybook
+    return (
+     <Grid container>
+       <Grid item xs={3}>
+         <OrgsGridItem
+           id={id}
+           isSub={isSub}
+           type={type}
+           trustLevel={trustLevel}
+           name={name}
+           subs={subs}
+           entityName={entityName}
+           entityTrustLevel={entityTrustLevel}
+         />
+       </Grid>
+     </Grid>
+    );
+  });
+

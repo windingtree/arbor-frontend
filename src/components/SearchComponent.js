fa6cc42515b5c0c17681e189c7b9513be6c24d91
ki11erdance <killerdance7@gmail.com>
2020-02-11 18:50:23 +0200

    Home screen started

---
+++ src/components/SearchComponent.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Button, InputAdornment, TextField, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+//icons
+import SearchIcon from '../assets/SvgComponents/SearchIcon';
+//styles && colors
+import colors from '../styles/colors';
+
+const styles = makeStyles({
+  searchInputWrapper: {
+    width: '70%',
+  },
+  searchInput: {
+    position: 'relative',
+    backgroundColor: colors.primary.white,
+    borderRadius: '8px 8px 0 0',
+    boxShadow: '0px 2px 6px rgba(10, 23, 51, 0.04), 0px 4px 12px rgba(10, 23, 51, 0.04)'
+  },
+  searchInputIcon: {
+    width: '14px',
+    height: '14px',
+    color: colors.greyScale.common,
+    transition: 'color .2s cubic-bezier(0.0, 0, 0.2, 1) 0ms'
+  },
+  searchButtonWrapper: {
+    position: 'absolute',
+    bottom: '0',
+    right: '0',
+    zIndex: 2
+  },
+  searchButton: {
+    backgroundImage: colors.gradients.green,
+    border: `1px solid ${colors.secondary.cyan}`,
+    borderRadius: '8px',
+    textTransform: 'none',
+    padding: '14px 26px'
+  },
+  searchButtonLabel: {
+    fontSize: '16px',
+    fontWeight: 600,
+    lineHeight: 1.24,
+    color: colors.primary.white
+  },
+});
+
+export default function SearchComponent(props) {
+  const classes = styles();
+  const { searchValue, handleSearchValue, fetchSearchResult } = props;
+
+  return (
+    <div className={classes.searchForm}>
+      <div className={classes.searchInputWrapper}>
+        <TextField
+          id={'search-input'}
+          autoComplete={' '}
+          className={classes.searchInput}
+          placeholder={`Search by organization name or ORG.ID`}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position={'start'}>
+                <SearchIcon className={classes.searchInputIcon} viewBox={'0 0 14 14'}/>
+              </InputAdornment>
+            )
+          }}
+          fullWidth
+          value={searchValue}
+          onChange={handleSearchValue}
+        />
+      </div>
+      <div className={classes.searchButtonWrapper}>
+        <Button onClick={fetchSearchResult} className={classes.searchButton}>
+          <Typography variant={'inherit'} className={classes.searchButtonLabel} noWrap>Find organization</Typography>
+        </Button>
+      </div>
+    </div>
+  )
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
+import history from '../redux/history';
+import { Container, Typography, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+//components
+import SearchComponent from '../components/SearchComponent';
+//icons && illustrations
+import HomeSearchIllustration from '../assets/SvgComponents/home-search-illustration.svg';
+import WhatIfIllustration from '../assets/SvgComponents/what-if-illust.svg';
+//styles && colors
+import colors from '../styles/colors';
+import { green } from '@material-ui/core/colors';
 
-import { Container, Typography } from '@material-ui/core';
+const styles = makeStyles({
+  searchContainer: {
+    backgroundColor: colors.secondary.yellowLight,
+    width: '100%'
+  },
+  searchContent: {
+    position: 'relative',
+    paddingTop: '122px',
+    paddingBottom: '122px',
+  },
+  searchTitle: {
+    fontSize: '40px',
+    fontWeight: 500,
+    lineHeight: 1.2,
+    color: colors.greyScale.darkest,
+    width: '50%'
+  },
+  subtitleWrapper: {
+    width: '50%',
+    margin: '20px 0 30px 0',
+  },
+  subtitle: {
+    fontSize: '15px',
+    fontWeight: 400,
+    lineHeight: 1.6,
+    color: colors.greyScale.dark,
+  },
+  searchForm: {
+    position: 'relative',
+    width: '60%',
+  },
+  illustrationWrapper: {
+    position: 'absolute',
+    top: '87px',
+    right: '-60px'
+  },
+  kybChecksContent: {
+    padding: '80px 0'
+  },
+  kybChecksImageWrapper: {
+    position: 'relative',
+    width: '40%',
+    minHeight: '360px'
+  },
+  kybChecksIllustration: {
+    position: 'absolute',
+    left: '-80px',
+    top: '0'
+  },
+  kybChecksTextContainer: {
+    position: 'relative',
+    width: '40%'
+  },
+  kybChecksTitle: {
+    fontSize: '32px',
+    fontWeight: 500,
+    lineHeight: 1.14,
+    color: colors.greyScale.darkest,
+    paddingBottom: '20px'
+  },
+  kybChecksSubtitle: {
+    fontSize: '16px',
+    fontWeight: 400,
+    lineHeight: 1.55,
+    color: colors.greyScale.dark
+  },
+});
 
 function Home() {
+  const classes = styles();
+  const [searchValue, setSearchValue] = useState('');
+
+
+  const handleSearch = event => {
+    setSearchValue(event.target.value);
+  };
+
   return (
-    <Container>
-      <Typography variant={'h3'}>
-        Home
-      </Typography>
-    </Container>
+    <div>
+      <div className={classes.searchContainer}>
+        <Container className={classes.searchContent}>
+          <div>
+            <Typography variant={'h2'} className={classes.searchTitle}>
+              Find trusted partners for your business
+            </Typography>
+            <div className={classes.subtitleWrapper}>
+              <Typography variant={'subtitle1'} className={classes.subtitle}>
+                Arbor lets companies exchange trusted data with open source solutions. So you would know ho you are dealing with. It is designed to make Know-Your-Business (KYB) processes cheaper, faster, and more secure.
+              </Typography>
+            </div>
+            <div className={classes.searchForm}>
+              <SearchComponent searchValue={searchValue} handleSearchValue={handleSearch} fetchSearchResult={() => history.push('search', { state: { request: searchValue }})}/>
+            </div>
+          </div>
+          <div className={classes.illustrationWrapper}>
+            <img src={HomeSearchIllustration} alt={'illustration'} />
+          </div>
+        </Container>
+      </div>
+      <div>
+        <Container>
+          <Grid container justify={'space-around'} alignItems={'center'} className={classes.kybChecksContent} wrap={'nowrap'}>
+            <Grid item className={classes.kybChecksImageWrapper}>
+              <img src={WhatIfIllustration} alt={'illustration'} className={classes.kybChecksIllustration}/>
+            </Grid>
+            <Grid item className={classes.kybChecksTextContainer}>
+              <Typography variant={'h3'} className={classes.kybChecksTitle}>
+                Simplify routine KYB checks
+              </Typography>
+              <Typography variant={'subtitle2'} className={classes.kybChecksSubtitle}>
+                Every organization in Arbor database has been validated via a secure process. It is transparent, decentralized and free of charge. Look for organizations across various industries in our database or join the community and get discovered by potential clients and partners.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    </div>
   )
 }
 

cbb85e6f4301aa030b5ef6196dd14e82494a2b4d
Kostya <kurinniy.k@gmail.com>
2020-02-26 17:14:38 +0200

    website cleanup2

--- src/routes/Trust/TrustWebsite.js
+++ src/routes/Trust/TrustWebsite.js
@@ -167,9 +167,9 @@ export const styles = makeStyles({
 });
 
 function TrustWebsite() {
-  const orgid = (!!history.location.state && !!history.location.state.orgid) ? history.location.state.orgid : false;
+  const id = (!!history.location.state && !!history.location.state.id) ? history.location.state.id : false;
   const website = _.get(history, 'location.state.website', 'example.com');
-  const txtFileData = `data:text/json;charset=utf-8,orgid=${orgid}`;
+  const txtFileData = `data:text/json;charset=utf-8,orgid=${id}`;
   const classes = styles();
 
   return (
@@ -307,7 +307,6 @@ function TrustWebsite() {
             </a>
           </Button>
         </Box>
-        }
       </Container>
     </div>
   )

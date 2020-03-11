a0ba718395b2bda49f32e62e8fc1af95f1c43cc6
ki11erdance <killerdance7@gmail.com>
2020-03-11 13:03:12 +0200

    ORGID-00 -- Organization -- return back as functional component => find issues when subsidiaries di not fetch

--- src/routes/Organization/Organization.js
+++ src/routes/Organization/Organization.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import { fetchOrganizationInfo, fetchOrganizationSubsInfo, selectItem, selectSubs } from '../../ducks/fetchOrganizationInfo';
@@ -10,42 +10,32 @@ import TodoList, { getTodo } from "./Components/TodoList";
 import Info from "./Components/Info";
 import SubOrganizations from "./Components/SubOrganizations";
 
-class Organization extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isTransactionSuccess: false,
-    }
-  }
+function Organization (props) {
+  const id = history.location.state ? history.location.state.id : history.location.pathname.split('/')[2];
+  const canManage = history.location.pathname !== `/organization/${id}`;
+  const { organization, subs } = props;
+  const subsidiaries = _.get(organization, 'subsidiaries', []);
+  const todos = getTodo(organization);
+  const agents = _.get(organization, 'jsonContent.publicKey', []);
+  const { owner } = organization;
 
-  componentDidMount() {
-    const id = history.location.state ? history.location.state.id : history.location.pathname.split('/')[2];
-    this.props.fetchOrganizationInfo({id});
+  useEffect(() => {
+    props.fetchOrganizationInfo({ id });
+  }, [id]); // eslint-disable-line react-hooks/exhaustive-deps
 
-    if(this.props.organization.subsidiaries && this.props.organization.subsidiaries.length) {
-      this.props.fetchOrganizationSubsInfo({id});
-    }
-  }
+  useEffect(() => {
+    subsidiaries && subsidiaries.length && props.fetchOrganizationSubsInfo({ id });
+  }, [id, subsidiaries]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  render() {
-    const id = history.location.state ? history.location.state.id : history.location.pathname.split('/')[2];
-    const canManage = history.location.pathname !== `/organization/${id}`;
-    const { organization, subs } = this.props;
-    const subsidiaries = _.get(organization, 'subsidiaries', []);
-    const agents = _.get(organization, 'jsonContent.publicKey', []);
-    const { owner } = organization;
-    const todos = getTodo(organization);
-
-    return (
-      <div>
-        <TopNavigation organization={organization} canManage={canManage} todos={todos}/>
-        <Info organization={organization} canManage={canManage}/>
-        {subsidiaries && subs && subs.length > 0 && <SubOrganizations organization={organization} subs={subs} canManage={canManage} />}
-        {canManage && <Agents owner={owner} agents={agents}/>}
-        {canManage && <TodoList organization={organization}/>}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <TopNavigation organization={organization} canManage={canManage} todos={todos}/>
+      <Info organization={organization} canManage={canManage}/>
+      {subsidiaries && subsidiaries.length > 0 && <SubOrganizations organization={organization} subs={subs} canManage={canManage} />}
+      {canManage && <Agents owner={owner} agents={agents}/>}
+      {canManage && <TodoList organization={organization}/>}
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,6 @@ export const LIF_DEPOSIT_AMOUNT = 1000;
 export const API_URI = `https://api.arbor.dev.bachoodesign.com/api/v1`;
 // export const API_URI = `https://192.168.88.71:3000/api/v1`;
 // export const API_URI = `https://127.0.0.1:3000/api/v1`;
-// export const API_URI = `https://loc.orangerea.com.ua:3333/api/v1`;
 // export const API_URI = `https://loc.orangerea.com.ua:3000/api/v1`;
 export const ORGID_PROXY_ADDRESS = '0xc8fD300bE7e4613bCa573ad820a6F1f0b915CfcA';
 export const ORGID_ABI = [

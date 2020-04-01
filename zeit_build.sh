fa91e76e84df504c5eefb86d7f7d6556a180eb02
Mathieu TAHON <mathieu.tahon@noath.net>
2020-04-01 10:55:15 +0200

    Adding debugging entry to know the folder path on Zeit

--- zeit_build.sh
+++ zeit_build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "NOW_GITHUB_COMMIT_REF : $NOW_GITHUB_COMMIT_REF"
 echo "NODE_ENV: $NODE_ENV"
+pwd
+ls -Al
+
 
 case "$NOW_GITHUB_COMMIT_REF" in
 staging)  echo "Building $NOW_GITHUB_COMMIT_REF"

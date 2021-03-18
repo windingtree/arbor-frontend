dba146d431b2f1e48275f1555a22cd19cf844cbc
Kostiantyn Smyrnov <kostysh@gmail.com>
2021-03-18 23:06:04 +0200

    fixed regex for telegram, discord and email

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,5 +1,5 @@
 module.exports.phone = /^([+]{0,1})([0-9- ]+)$/;
 module.exports.url = /^(?:^|\s)((https?:\/\/)?(?:localhost|[\w-]+(?:\.[\w-]+)+)(:\d+)?(\/\S*)?)$/;
-module.exports.email = /^[\w.-]+@[a-zA-Z._-]+?\.[a-zA-Z]{2,3}$/;
-module.exports.at = /^@[a-zA-Z._-]+$/;
-module.exports.discord = /^[a-zA-Z._-]+#\d+$/;
+module.exports.email = /^[\w.-]+@[0-9a-zA-Z._-]+?\.[a-zA-Z]{2,3}$/;
+module.exports.at = /^@[0-9a-zA-Z._-]+$/;
+module.exports.discord = /^[0-9a-zA-Z._-]+#\d+$/;
